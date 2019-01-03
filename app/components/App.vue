75f413d3e71b57cf008c7f1745def10734144836
htbkoo <h1017760@connect.hku.hk>
2019-01-03 22:47:29 +0800

    made multiple tabs according to places in `App.vue` htbkoo/hey-hkul-hours-native-ns-vue

--- app/components/App.vue
+++ app/components/App.vue
@@ -5,38 +5,54 @@
                  android:tabTextColor="#c4ffdf"
                  android:selectedTabTextColor="#ffffff"
                  androidSelectedTabHighlightColor="#ffffff">
-            <TabViewItem title="Tab 1">
-                <GridLayout columns="*" rows="*">
-                    <Label class="message" :text="msg" col="0" row="0"/>
+            <!--
+            <Place
+                    v-for="place in places"
+                    v-bind:key="place.id"
+                    v-bind:meta="place.meta"
+                    v-bind:banner="place.banner"
+                    v-bind:libraries="place.libraries"
+            />
+            -->
+
+            <TabViewItem
+                    v-for="place in places"
+                    v-bind:key="place.id"
+                    :title="place.meta.name">
+                <StackLayout columns="*" rows="*">
+                    <Label class="message" :text="place.meta.location" col="0" row="0"/>
                     <!--<Image src="res://icon" stretch="aspectFill" />-->
-                    <Image src="~/assets/images/hkul/wikipedia/hkul_banner.jpg" stretch="aspectFit"/>
-                </GridLayout>
-            </TabViewItem>
-            <TabViewItem title="Tab 2">
-                <GridLayout columns="*" rows="*">
-                    <Label class="message" text="Tab 2 Content" col="0" row="0"/>
-                    <DatePicker year="2018" month="1" day="15" minDate="1970-01-01" maxDate="2100-12-31"/>
-                </GridLayout>
-            </TabViewItem>
-            <TabViewItem title="Tab 3">
-                <GridLayout columns="*" rows="*">
-                    <Label class="message" text="Tab 3 Content" col="0" row="0"/>
-                </GridLayout>
+                    <!--<Image :src="require(`@/assets/images/${banner.src}`)" stretch="aspectFit"/>-->
+                    <Image :src="`@/assets/images/${place.banner.src}`" :alt="place.banner.alt" stretch="aspectFit"/>
+                </StackLayout>
             </TabViewItem>
         </TabView>
     </Page>
 </template>
 
 <script lang="ts">
+    import Place from "@/components/Place.vue"
+    import {LibraryProps} from "../types/LibraryProps";
+
+    const libraries: LibraryProps[] = [];
+
     export default {
-        data() {
-            return {
-                msg: 'Hello World!'
-            }
+        components: {
+            Place
         },
         props: {
-            title: {
-                type: String
+            title: {type: String,}
+        },
+        data() {
+            return {
+                places: [
+                    {
+                        id: 1,
+                        meta: {name: "HKU Library", location: "Pok Fu Lam"},
+                        banner: {src: "hkul/wikipedia/hkul_banner.jpg", alt: "HKU Main Library"},
+                        libraries
+                    },
+                ]
             }
         }
     }
