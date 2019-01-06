23ce393aa39a391f50f2d003445b1d9284033f4c
htbkoo <h1017760@connect.hku.hk>
2019-01-06 17:20:08 +0800

    refactored `NativeHtmlParser.ts` htbkoo/hey-hkul-hours-native-ns-vue

--- app/services/NativeHtmlParser.ts
+++ app/services/NativeHtmlParser.ts
@@ -5,31 +5,42 @@ import {HtmlParser} from "hey-hkul-hours";
 
 export default class NativeHtmlParser implements HtmlParser {
     async parseHtml(html: string): Promise<RawStringsMap> {
-        return new Promise((resolve, reject) => {
-            parseString(html, {explicitChildren: true, ignoreAttrs: true}, (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    console.log(result);
-                    resolve(result);
-                }
-            });
-        })
-            .then(result => (result as any).div.$$.table[0].$$.tr)
-            .then(trs => trs.map(tr=>tr.$$))
-            .then(rows => rows.reduce((obj, row) => {
-                const [key, value] = row.td;
+        return promiseParseString(html)
+            .then(extractAllRows)
+            .then(rows => rows.reduce(mapRowsToStringsMap, {}));
 
-                // typeof result.div.table[0].tr[0].td[0]
-                if (typeof key === "string") {
-                    obj[key] = value;
-                } else if (typeof key === "object") {
-                    obj[key._] = value._;
-                } else {
-                    throw new Error(`Unable to parse key:"${key}" and value:"${value}" from html:"${html}"`);
-                }
+        function promiseParseString(html: string) {
+            return new Promise((resolve, reject) =>
+                parseString(html, {explicitChildren: true, ignoreAttrs: true}, (err, result) => {
+                    if (err) {
+                        console.log(`Error caught @ NativeHtmlParser.parseHtml: ${err}`);
+                        reject(err);
+                    } else {
+                        console.log(`Parsing html @ NativeHtmlParser.parseHtml:${result}`);
+                        resolve(result);
+                    }
+                })
+            )
+        }
 
-                return obj;
-            }, {}));
+        function extractAllRows(result) {
+            const allTrs = (result as any).div.$$.table[0].$$.tr;
+            return allTrs.map(tr => tr.$$);
+        }
+
+        function mapRowsToStringsMap(obj, row) {
+            const [key, value] = row.td;
+
+            // typeof result.div.table[0].tr[0].td[0]
+            if (typeof key === "string") {
+                obj[key] = value;
+            } else if (typeof key === "object") {
+                obj[key._] = value._;
+            } else {
+                throw new Error(`Unable to parse key:"${key}" and value:"${value}" from html:"${html}"`);
+            }
+
+            return obj;
+        }
     }
 };
