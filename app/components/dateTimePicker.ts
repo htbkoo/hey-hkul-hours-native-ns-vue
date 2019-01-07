84c5a6e1d53b480feed6fa45d01ae24a573b35f9
htbkoo <h1017760@connect.hku.hk>
2019-01-07 21:55:53 +0800

    refactored and cleaned up dateTimePicker.ts htbkoo/hey-hkul-hours-native-ns-vue

--- app/components/dateTimePicker.ts
+++ app/components/dateTimePicker.ts
@@ -1,39 +1,23 @@
 import {ModalDatetimepicker} from "nativescript-modal-datetimepicker";
 import moment from "moment";
 
+const TODAY = new Date();
+
 const picker = new ModalDatetimepicker();
 
 export default {
-    async showDateModal() {
+    async showDateModal(date = TODAY) {
         return picker.pickDate({
             title: "Select Date", // iOS only
             theme: "light", // ios only
+            startingDate: date,
             datePickerMode: "calendar",
         }).then(result => {
             // Note the month is 1-12 (unlike js which is 0-11)
             const date = `${result.day}-${result.month}-${result.year}`;
             console.log(`Picked date is: ${date}`);
 
-            // var jsdate = new Date(result.year, result.month - 1, result.day);
-            // this.selectedDate = moment(date, "d-M-YYYY");
             return moment(date, "D-M-YYYY");
         });
     }
 }
-
-// Pick Date
-// exports.selectDate = function() {};
-
-/*
-// Pick Time
-exports.selectTime = function() {
-    picker
-        .pickTime()
-        .then(result => {
-            console.log("Time is: " + result.hour + ":" + result.minute);
-        })
-        .catch(error => {
-            console.log("Error: " + error);
-        });
-};
-*/
