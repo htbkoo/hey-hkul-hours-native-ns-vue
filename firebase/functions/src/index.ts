5d739e0d1a4b4130e10a98f94f2429d90bc63bd1
htbkoo <h1017760@connect.hku.hk>
2019-01-13 13:25:41 +0800

    auto-formatted htbkoo/hey-hkul-hours-native-ns-vue

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -4,6 +4,6 @@ import * as functions from 'firebase-functions';
 // // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    response.send("Hello from Firebase!");
 });
 
