e570132a0482688a7294e9d350c5b8157ab44ad6
htbkoo <h1017760@connect.hku.hk>
2019-01-15 22:42:10 +0800

    updated logic in index.ts htbkoo/hey-hkul-hours-native-ns-vue

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,8 +2,17 @@ import * as functions from 'firebase-functions';
 import firebaseAdmin from "firebase-admin";
 import moment from "moment";
 import {HEY_HKUL_HOURS_FCM_TOPIC_NAME} from "../../../constants/firebaseCloudMessaging";
+import {HkuLibraryHoursFetcher} from "hey-hkul-hours";
+import LibraryHours from "hey-hkul-hours/dist/service/hour/model/LibraryHours";
+import Hours from "hey-hkul-hours/dist/service/hour/model/Hours";
+import Session from "../../../app/services/SessionHoursFormatter";
 
 firebaseAdmin.initializeApp(functions.config().firebase);
+
+const hkuLibraryHoursFetcher = new HkuLibraryHoursFetcher();
+const formatter = new Session();
+
+const DEFAULT_ZONE = "Main Library";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -23,23 +32,21 @@ export const hourly_job = functions.pubsub
 
 export const push_notification_daily_open_hours = functions.pubsub
     .topic('daily-tick')
-    .onPublish((message) => {
-        console.log(`Received daily tick, pushing notification through FCM for the opening hours for today: ${moment().format("DD-MM-YYYY")}`);
+    .onPublish(message => {
+        const now = moment();
+        const todayString = now.format("DD-MM-YYYY");
+
+        console.log(`Received daily tick, pushing notification through FCM for the opening hours for today: ${todayString}`);
         processTickMessage(message);
 
-        const today = moment().format("DD-MM-YYYY");
+        return hkuLibraryHoursFetcher.retrieveHours(now)
+            .then(hours => {
+                const payload = createPayloadFromHours(hours);
 
-        const payload = {
-            notification: {
-                title: `hey-hkul-hours: ${today}`,
-                body: `hello, try day: ${today}`
-            },
-            data: {}
-        };
-
-        return firebaseAdmin.messaging().sendToTopic(HEY_HKUL_HOURS_FCM_TOPIC_NAME, payload)
+                return firebaseAdmin.messaging().sendToTopic(HEY_HKUL_HOURS_FCM_TOPIC_NAME, payload)
+            })
             .then(result => {
-                console.log(`Finished pushing notification through FCM for the opening hours for today: ${today}`);
+                console.log(`Finished pushing notification through FCM for the opening hours for today: ${todayString}`);
                 return result;
             });
     });
@@ -48,5 +55,70 @@ function processTickMessage(message) {
     if (message.data) {
         const dataString = Buffer.from(message.data, 'base64').toString();
         console.log(`Message Data: ${dataString}`);
+    }
+}
+
+function createPayloadFromHours(hours: LibraryHours) {
+    const actualDate = hours.getDate();
+    const allZonesHours = hours.getHoursForAllZones();
+    if (isObjectEmpty(allZonesHours)) {
+        console.log("ERROR - no close time available");
+        return getPayloadForNoHoursError(actualDate);
+    } else {
+        console.log(`Building notification payload for ${actualDate}`);
+        return {
+            notification: {
+                title: buildTitle(allZonesHours),
+                body: buildBody(allZonesHours, actualDate)
+            },
+            data: {}
+        };
+
+    }
+}
+
+function getPayloadForNoHoursError(actualDate) {
+    return {
+        notification: {
+            title: `${actualDate} - No library Hours available`,
+            body: `Please report to "https://github.com/htbkoo/hey-hkul-hours-native-ns-vue/issues"`
+        },
+        data: {}
+    };
+}
+
+function buildTitle(allZonesHours) {
+    const allZoneNames = Object.keys(allZonesHours);
+    const titleZoneName = (DEFAULT_ZONE in allZonesHours)
+        ? DEFAULT_ZONE
+        : allZoneNames[0];
+    const titleZoneHours = allZonesHours[titleZoneName];
+    const titleZoneTime = zoneHoursAsString(titleZoneHours);
+
+    return `${titleZoneTime} (${titleZoneName})`;
+}
+
+function buildBody(allZonesHours, actualDate) {
+    const allZoneNames = Object.keys(allZonesHours);
+    const allZoneTimes = allZoneNames.map(name => allZonesHours[name])
+        .map(zoneHoursAsString)
+        .join("\n");
+
+    return `${actualDate}
+${allZoneTimes}`;
+}
+
+function isObjectEmpty(obj) {
+    return (Object.keys(obj).length === 0) && (obj.constructor === Object);
+}
+
+function zoneHoursAsString(zoneHours: Hours) {
+    if (zoneHours.isClosed()) {
+        return "Closed";
+    } else {
+        return zoneHours.asArray()
+            .map(formatter.format)
+            .map(({from, to}) => `${from}-${to}`)
+            .join(", ");
     }
 }
