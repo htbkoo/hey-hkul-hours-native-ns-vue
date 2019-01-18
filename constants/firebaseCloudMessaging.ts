520a6d4ea79de110aba347a417b56fdf064771e3
htbkoo <h1017760@connect.hku.hk>
2019-01-18 15:37:09 +0800

    added hourly topic name htbkoo/hey-hkul-hours-native-ns-vue

--- constants/firebaseCloudMessaging.ts
+++ constants/firebaseCloudMessaging.ts
@@ -1,1 +1,7 @@
+const HEY_HKUL_HOURS_FCM_TOPIC_NAME_HOURLY = "hey_hkul_hours_fcm_hourly";
+
+export function getHourlyTopicName(hourInDay) {
+    return `${HEY_HKUL_HOURS_FCM_TOPIC_NAME_HOURLY}_${hourInDay}`;
+}
+
 export const HEY_HKUL_HOURS_FCM_TOPIC_NAME = "hey_hkul_hours_fcm_daily";
